db56722b31459bd242a1cd00764b43eff098234c
dayana-sog <dayanasog23@gmail.com>
2020-05-23 17:45:15 +0100

    Estilização da buttom-tabs

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
-  );
+// import { Container } from './styles';
+
+import App from './src';
+
+function Main () {
+  return <App />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Main;
