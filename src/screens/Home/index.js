40cb8c647f1b5ff68e02738c9e73ccd78a00374c
dayana-sog <dayanasog23@gmail.com>
2020-05-23 18:55:44 +0100

    Estilização das Suggestions

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native';
-// import { Container } from './styles';
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
+
+import { 
+  Wrapper, 
+  Container,
+  Header,
+  BalanceContainer,
+  BalanceTitle,
+  Balance
+} from './styles';
+
+import Suggestions from '../../components/Suggestions';
 
 function Home() {
-  return <Text>Home</Text>;
+  return (
+    <Wrapper>
+      <Container>
+        <Header>
+          <MaterialCommunityIcons name="qrcode-scan" size={30} color="#10c86e" />
+
+          <BalanceContainer>
+            <BalanceTitle>Meu Saldo</BalanceTitle>
+            <Balance>R$ 0,00</Balance>
+          </BalanceContainer>
+
+          <AntDesign name="gift" size={30} color="#10c86e" />
+        </Header>
+
+        <Suggestions />
+      </Container>
+    </Wrapper>
+  );
 }
 
 export default Home;
